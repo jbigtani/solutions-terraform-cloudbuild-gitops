fa91fb1f1c54376acf6ee2b607de269d311bf3b5
jbigtani <jitendra.bigtani@gmail.com>
2021-07-27 15:28:24 +0530

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "disco-torus-320508-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="disco-torus-320508"
